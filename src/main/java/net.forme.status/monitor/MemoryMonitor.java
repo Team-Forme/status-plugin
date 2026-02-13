338d840fc9cd2c2bb46fd5bdfe73edaec00c5c2f
moguma <kitsuneko.info@gmail.com>
2026-02-14 01:44:59 +0900

    Create MemoryMonitor class for memory usage tracking
    Add MemoryMonitor class to track memory usage

--- src/main/java/net.forme.status/monitor/MemoryMonitor.java
+++ src/main/java/net.forme.status/monitor/MemoryMonitor.java
@@ -1,1 +1,11 @@
+package net.forme.status.monitor;
 
+public class MemoryMonitor {
+
+    public String getMemoryUsage() {
+        long used = (Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory()) / 1024 / 1024;
+        long max = Runtime.getRuntime().maxMemory() / 1024 / 1024;
+        return used + "MB / " + max + "MB";
+    }
+}
