f201fb9227dccf9b032c6a547875e07bc8d65002
moguma <kitsuneko.info@gmail.com>
2026-02-14 01:44:15 +0900

    Implement core functionality for FormeStatus plugin

--- src/main/java/net.forme.status/FormeStatus.java
+++ src/main/java/net.forme.status/FormeStatus.java
@@ -1,1 +1,46 @@
+package net.forme.status;
 
+import net.forme.status.alert.AlertManager;
+import net.forme.status.api.StatusAPI;
+import net.forme.status.commands.StatusCommand;
+import net.forme.status.listeners.ServerPingListener;
+import net.forme.status.monitor.*;
+import net.forme.status.storage.SQLiteManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FormeStatus extends JavaPlugin {
+
+    private TPSMonitor tpsMonitor;
+    private MemoryMonitor memoryMonitor;
+    private UptimeMonitor uptimeMonitor;
+    private AlertManager alertManager;
+    private SQLiteManager sqliteManager;
+    private StatusAPI api;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        tpsMonitor = new TPSMonitor();
+        memoryMonitor = new MemoryMonitor();
+        uptimeMonitor = new UptimeMonitor();
+        sqliteManager = new SQLiteManager(this);
+        alertManager = new AlertManager(this, tpsMonitor);
+        api = new StatusAPI(this);
+
+        getCommand("status").setExecutor(
+                new StatusCommand(tpsMonitor, memoryMonitor, uptimeMonitor));
+
+        getServer().getPluginManager()
+                .registerEvents(new ServerPingListener(this, tpsMonitor), this);
+
+        alertManager.start();
+        sqliteManager.init();
+
+        if (getConfig().getBoolean("api.enabled")) {
+            api.start();
+        }
+
+        getLogger().info("FormeStatus Enabled");
+    }
+}
