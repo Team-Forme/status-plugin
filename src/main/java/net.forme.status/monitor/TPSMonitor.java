1a3a02122deef702480b86f2c7d6a454e8768f14
moguma <kitsuneko.info@gmail.com>
2026-02-14 01:44:40 +0900

    Add TPSMonitor class to monitor server performance

--- src/main/java/net.forme.status/monitor/TPSMonitor.java
+++ src/main/java/net.forme.status/monitor/TPSMonitor.java
@@ -1,1 +1,18 @@
+package net.forme.status.monitor;
 
+import org.bukkit.Bukkit;
+
+public class TPSMonitor {
+
+    public double getTPS() {
+        return Bukkit.getServer().getTPS()[0];
+    }
+
+    public int getOnline() {
+        return Bukkit.getOnlinePlayers().size();
+    }
+
+    public int getMax() {
+        return Bukkit.getMaxPlayers();
+    }
+}
