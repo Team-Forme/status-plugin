a7007c8a752cd5fcaaac1fcc15b5cb5b7a84ff02
moguma <kitsuneko.info@gmail.com>
2026-02-14 01:45:22 +0900

    Implement uptime retrieval in UptimeMonitor
    Add method to retrieve system uptime in hours and minutes.

--- src/main/java/net.forme.status/monitor/UptimeMonitor.java
+++ src/main/java/net.forme.status/monitor/UptimeMonitor.java
@@ -1,1 +1,14 @@
+package net.forme.status.monitor;
 
+import java.lang.management.ManagementFactory;
+
+public class UptimeMonitor {
+
+    public String getUptime() {
+        long ms = ManagementFactory.getRuntimeMXBean().getUptime();
+        long sec = ms / 1000;
+        long h = sec / 3600;
+        long m = (sec % 3600) / 60;
+        return h + "h " + m + "m";
+    }
+}
